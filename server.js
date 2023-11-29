0e1055be38a7a4e7ff779d395d454496302ea4b9
SuperCoder <propscode254@gmail.com>
2023-11-29 16:22:00 +0100

    works

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
 import express from "express";
 import path from "path";
 
-const PORT = 9999;
+const PORT = 9898;
 const app = express();
 
 app.use(express.static("assets"));
 
 app.use(express.static("pages"));
 app.use(express.static("img"));
-
-app.get("/*.html", (req, res) => {
-  const requestedPage = req.params[0];
-  res.sendFile(path.join(__dirname, "pages", requestedPage));
-});
 
 app.listen(PORT, () => console.log("port " + PORT));
